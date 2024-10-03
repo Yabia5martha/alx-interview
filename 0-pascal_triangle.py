e6f81ba8beb2545aeeb6bda04ee4759ab50707f4
Martha Siiyabia Tong <marthasiiyatong@gmail.com>
2024-10-02 22:35:25 -0700

    pascal_triangle

--- 0-pascal_triangle.py
+++ 0-pascal_triangle.py
@@ -1,24 +1,17 @@
 #!/usr/bin/python3
-"""
-0-pascal_triangle
-"""
-
-
 def pascal_triangle(n):
-    """
-    Returns a list of integers
-    representing the Pascal Triangle of n
-    returns empty list if n <= 0
-    """
-    k = []
+
+    matrix = []
+
     if n <= 0:
-        return k
-    k = [[1]]
-    for i in range(1, n):
-        temp = [1]
-        for j in range(len(k[i - 1]) - 1):
-            curr = k[i - 1]
-            temp.append(k[i - 1][j] + k[i - 1][j + 1])
+        return matrix     
+    matrix = [[1]] 
+
+        for i in range(1, n):
+        temp = [1] 
+                for j in range(len(matrix[i - 1]) - 1):
+            curr = matrix[i - 1]
+            temp.append(matrix[i - 1][j] + matrix[i - 1][j + 1])
         temp.append(1)
-        k.append(temp)
-    return k
+        matrix.append(temp)
+    return matrix
